e45a54b53de27ac1f106e8b9382c6a091781fbc6
Mulognikov <mulognikov629@gmail.com>
2020-11-02 12:11:15 +0500

    Add tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour, IListener
+{
+    void Start()
+    {
+        int tutorialState = PlayerPrefs.GetInt("TutorialState");
+
+        if (tutorialState > 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        EventManager.Instance.AddListener(EventTypes.SuccessJump, this);
+    }
+
+    public void OnEvent(EventTypes eventType, Component sender, object param = null)
+    {
+        switch (eventType)
+        {
+            case EventTypes.SuccessJump:
+                OnSuccessJump();
+                break;
+        }
+    }
+
+    private void OnSuccessJump()
+	{
+        StartCoroutine(TutorialComplited());
+	}
+
+    private IEnumerator TutorialComplited()
+	{
+        PlayerPrefs.SetInt("TutorialState", 1);
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+        EventManager.Instance.RemoveRedundancies();
+    }
+}
